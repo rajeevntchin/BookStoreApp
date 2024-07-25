7b8caf5821adeb86a5f47bc55a8bb8b661ff26f6
Rajeev <rajeevnarayantewari760@gmail.com>
2024-07-25 12:55:13 +0530

    Backend Done!

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import list from "../../public/list.json";
 import Card from "./Card";
+import axios from "axios";
 import {Link} from "react-router-dom"
 function Course() {
+    const [book, setBook] = useState([]);
+    useEffect(() => {
+        const getBook = async () => {
+            try{
+                const res = await axios.get("https://localhost:4001/book");
+                console.log(res.data);
+                setBook(res.data);
+                }catch(error){
+                    console.log(error);
+            }
+        }
+        getBook();
+    },[])
   return ( 
 <>
 <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 ">
--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect,useState}  from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json";
+
+import axios from "axios";
 import Card from "./Card";
 function Freebook() {
-    const filterData =list.filter((data) => data.category === "Free");
+  const [book, setBook] = useState([]);
+    useEffect(() => {
+        const getBook = async () => {
+            try{
+                const res = await axios.get("http://localhost:4001/book");
+                console.log(res.data);
+                setBook(res.data.filter((data) => data.category === "Free"));
+                }catch(error){
+                    console.log(error);
+            }
+        }
+        getBook();
+    },[]);
+     
 
     var settings = {
         dots: true,
@@ -54,7 +68,7 @@ function Freebook() {
     
     <div>
     <Slider {...settings}>
-        {filterData.map((item) =>(
+        {book.map((item) =>(
             <Card item={item} key={item.id}/>
         ))}
       </Slider>
